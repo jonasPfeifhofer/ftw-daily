b275e77c31c911f0b6b06a35b3ebb2365932456a
Vesa Luusua <vesa.luusua@gmail.com>
2021-02-08 19:12:22 +0200

    Refactor EmailVerificationPage route config.

--- src/routeConfiguration.js
+++ src/routeConfiguration.js
@@ -340,8 +340,8 @@ const routeConfiguration = () => {
       name: 'EmailVerificationPage',
       auth: true,
       authPage: 'LoginPage',
-      component: props => <EmailVerificationPage {...props} />,
-      loadData: EmailVerificationPage.loadData,
+      component: EmailVerificationPage,
+      loadData: pageDataLoadingAPI.EmailVerificationPage.loadData,
     },
   ];
 };
