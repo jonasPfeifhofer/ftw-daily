9d1c0b2608f87ec17cf54f72ee658fceb6e3085e
Vesa Luusua <vesa.luusua@gmail.com>
2021-02-08 18:48:21 +0200

    Refactor SearchPage route config. Export loadData from SearchPage.duck.js file

--- src/containers/pageDataLoadingAPI.js
+++ src/containers/pageDataLoadingAPI.js
@@ -1,9 +1,14 @@
 /**
  * Export loadData calls from ducks modules of different containers
  */
+import { loadData as SearchPageLoader } from './SearchPage/SearchPage.duck';
 
 const pageDataLoadingAPI = () => {
-  return {};
+  return {
+    SearchPage: {
+      loadData: SearchPageLoader,
+    },
+  };
 };
 
 export default pageDataLoadingAPI();
